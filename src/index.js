9066d81bb68de19c488da37d30d01771ba41f6f7
BR2E <Efren.BR2E@gmail.com>
2023-01-18 17:55:28 -0700

    Agregando webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const email = document.getElementById('email');
+const contrasena = document.getElementById('contrasena');
+const boton = document.querySelector('.form__formulario--boton');
